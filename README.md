50d3dff2a9066b4a5b6cd88a4d1417c2882611e5
Andrew Keiser <webdev410@gmail.com>
2021-07-23 20:20:53 -0700

    working. still need to add license badges

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# readme-generator
+
+#Laurel's App
+
+##Description
+This app makes you better
+
+##Installation Instructions
+Open it. Run it.
+
+##Usage Instructions
+Use wisely
+
+##Contribution Guidelines
+Contibute often
+
+##Testing Instructions
+testing
--- index.js
+++ index.js
@@ -1,13 +1,116 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fileName = "README.md"
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const promptUser = () => {
+        return inquirer.prompt([
+                {
+                        type: 'input',
+                        message: 'Project Title: ',
+                        name: 'title',
+                },
+                {
+                        type: 'input',
+                        message: 'Description: ',
+                        name: 'description',
+                },
+                {
+                        type: 'input',
+                        message: 'Installation Instructions: ',
+                        name: 'install',
+                },
+                {
+                        type: 'input',
+                        message: 'Usage Information: ',
+                        name: 'usage',
+                },
+                {
+                        type: 'input',
+                        message: 'Contribution Guidelines: ',
+                        name: 'contribution',
+                },
+                {
+                        type: 'input',
+                        message: 'Test Instructions: ',
+                        name: 'test',
+                },
+        ]);
+
+
+
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, response) {
+        fs.writeFile(fileName, generateMarkdown(response), (error) => { /* handle error */ });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+        promptUser()
+                .then((response) => writeToFile("README.md", response))
+                .then(() => console.log('Successfully wrote to README.md'))
+                .catch((err) => console.error(err));
+
+              
+
+}
 
 // Function call to initialize app
 init();
+
+
+// ######################################################################################################################################################################################################################################################################
+
+// inquirer
+//         .prompt([
+//                 {
+//                         type: 'input',
+//                         message: 'Project Title: ',
+//                         name: 'title',
+//                 },
+//                 {
+//                         type: 'input',
+//                         message: 'Description: ',
+//                         name: 'description',
+//                 },
+//                 {
+//                         type: 'input',
+//                         message: 'Installation Instructions: ',
+//                         name: 'install',
+//                 },
+//                 {
+//                         type: 'input',
+//                         message: 'Usage Information: ',
+//                         name: 'usage',
+//                 },
+//                 {
+//                         type: 'input',
+//                         message: 'Contribution Guidelines: ',
+//                         name: 'contribution',
+//                 },
+//                 {
+//                         type: 'input',
+//                         message: 'Test Instructions: ',
+//                         name: 'test',
+//                 },
+//         ])
+//         .then((response) => {
+//                 const filename = `${response.title.toLowerCase().split(' ').join('')}.md`;
+
+//                 const readmeContent = `
+//                 # ${response.title}
+//                 ## Description: ${response.description} 
+//                 ## Install: ${response.install} 
+//                 ## Usage: ${response.usage} 
+//                 ## Constibution: ${response.contribution} 
+//                 ## Testing Instructions: ${response.test} `
+
+//                 fs.writeFile(filename, readmeContent, (error) => { /* handle error */ });
+//         });
+
+
+
